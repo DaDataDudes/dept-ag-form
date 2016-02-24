de98ddb95d84ff32b933d0421266f49c6b35b702
Bryan Butteling <Boots@Bryans-MacBook-Air.local>
2016-02-23 19:54:08 -1000

    setup test environment

---
+++ test/components/intro_page_spec.js
@@ -0,0 +1,8 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+var ReactTestUtils = require('react-addons-test-utils');
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('intro_page')
