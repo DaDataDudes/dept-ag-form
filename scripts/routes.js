26053e99a5aa8b00f4dc688650a5a18d4a0e2ce1
Brock <brocklanoza@gmail.com>
2016-02-25 22:11:51 -1000

    created container directories for other two views

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { FormCont } from './containers/index';
+import { FormCont, ThankYou } from './containers/index';
 import { Router, Route, browserHistory } from 'react-router';
 
 export default () => {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={FormCont} />
+      <Route path='/thankyou' component={ThankYou} />
     </Router>
   );
 };
