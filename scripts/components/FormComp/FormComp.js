531844777b75be07f5c146a8b510d58a0eca83e9
kawika <kawikakekahuna@gmail.com>
2016-03-04 18:40:26 -1000

    added tests for declaring plants, animals and checking for a 'none of the above'

--- scripts/components/FormComp/FormComp.js
+++ scripts/components/FormComp/FormComp.js
@@ -4,6 +4,26 @@ export default function ({ submitForm }) {
   return (
     <div className="main-form">
       <form onSubmit={submitForm}>
+        <select name="plantSelection" className="plant-selection">
+          <option value="fruitVeggie">Fresh Fruit & Vegetables</option>
+          <option value="dirt">Soil, Growing Media, Sand, etc</option>
+          <option value="flowers">Cut Flowers & Foilage</option>
+          <option value="seafood">Live Seafood(lobsters, clams, oysters, etc.)</option>
+          <option value="plants">Rooted Plants & Plant Cuttings, or Algae</option>
+          <option value="germs">Cultures of Bacteria, Fungi, Viruses, or Protozoa</option>
+          <option value="seeds">Raw or Propagative Seeds or Bulbs</option>
+          <option value="wildAnimals">Insects, Liva Fishes, Amphibians, etc</option>
+        </select>
+        <select name="animalSelection" className="animal-selection">
+          <option value="dogs">Dogs</option>
+          <option value="cats">Cats</option>
+          <option value="birds">Birds</option>
+          <option value="reptiles">Reptiles (Turtles, Lizards, Snakes, etc)</option>
+          <option value="otherAnimals">Other Animals</option>
+        </select>
+        <select name="none" className="none">
+          <option value="noneOfTheAbove">None Of The Above</option>
+        </select>
         <input type="text" name="fullName" />
         <input type="text" name="homeAddress" />
         <input type="text" name="city" />
--- test/components/FormComp_spec.js
+++ test/components/FormComp_spec.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithTag,
+  // scryRenderedDOMComponentsWithTag,
   findRenderedDOMComponentWithClass,
-  scryRenderedComponentsWithType,
-  Simulate,
+  // scryRenderedDOMComponentsWithClass,
+  // scryRenderedComponentsWithType,
+  // Simulate,
 } from 'react-addons-test-utils';
 import { expect } from 'chai';
-import FormComp from '../../scripts/components/FormComp/FormComp.js';
+import FormCont from '../../scripts/containers/FormCont/FormCont.js';
 
-describe('FromComp', () => {
-  const formComp = renderIntoDocument(
-    <FormComp />
+describe('FormCont', () => {
+  const formCont = renderIntoDocument(
+    <FormCont />
   );
   it('should declare plants', () => {
-    const plantSelection = findRenderedDOMComponentWithClass(formComp, 'plantSelection');
+    const plantSelection = findRenderedDOMComponentWithClass(formCont, 'plant-selection');
+    const plantOptions = plantSelection.querySelectorAll('option');
 
-    expect(plantSelection).to.be(false);
+    expect(plantOptions.length).to.equal(8);
+  });
+  it('should declare animals', () => {
+    const animalSelection = findRenderedDOMComponentWithClass(formCont, 'animal-selection');
+    const animalOptions = animalSelection.querySelectorAll('option');
+
+    expect(animalOptions.length).to.equal(5);
+  });
+  it('should have a checkbox that checks for none of the above', () => {
+    const noSelection = findRenderedDOMComponentWithClass(formCont, 'none');
+    const noSelectionOptions = noSelection.querySelectorAll('option');
+
+    expect(noSelectionOptions.length).to.equal(1);
   });
 });
