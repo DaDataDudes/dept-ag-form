88c51f7d907455500c3fd8b21ec4748e0190ba71
Bryan Butteling <Boots@Bryans-MacBook-Air.local>
2016-05-17 18:26:15 -1000

    Adding additional input fields
    Street Address and City

--- imports/client/containers/formPage.js
+++ imports/client/containers/formPage.js
@@ -5,15 +5,34 @@ import { reduxForm } from 'redux-form';
 class FormPage extends Component {
 
   render() {
-    const { fields: { firstName, lastName, email }, handleSubmit } = this.props;
+    const { fields: { firstName, lastName, email, address, city }, handleSubmit } = this.props;
 
     return (
       <div>
-        <header>Form Page</header>
+        <header>Contact Information</header>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="First Name" {...firstName}></input>
-          <input type="text" placeholder="Last Name" {...lastName}></input>
-          <input type="email" placeholder="Email" {...email}></input>
+          <div>
+            <label>First Name</label>
+            <div>
+              <input type="text" placeholder="First Name" {...firstName}></input>
+            </div>
+            <label>Last Name</label>
+            <div>
+              <input type="text" placeholder="Last Name" {...lastName}></input>
+            </div>
+          </div>
+          <div>
+            <label>Street Address</label>
+            <input type="text" placeholder="Address" {...address}></input>
+          </div>
+          <div>
+            <label>City</label>
+            <input type="text" placeholder="City" {...city}></input>
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="email" placeholder="Email" {...email}></input>
+          </div>
           <button type="submit">Submit</button>
         </form>
       </div>
@@ -23,5 +42,5 @@ class FormPage extends Component {
 
 export default reduxForm({
   form: 'departmentOfAg',
-  fields: ['firstName', 'lastName', 'email']
+  fields: ['firstName', 'lastName', 'email', 'address', 'city']
 })(FormPage);
