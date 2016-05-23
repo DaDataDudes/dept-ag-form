c4fdc0889d15c9954f3fec4534c1e8777461997b
Bryan Butteling <Boots@Bryans-MacBook-Air.local>
2016-05-22 18:55:35 -1000

    Added all contacts fields for form

--- imports/client/containers/formPage.js
+++ imports/client/containers/formPage.js
@@ -2,10 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import { browserHistory } from 'react-router';
 import { reduxForm } from 'redux-form';
 
+const islands = ['Oahu', 'Maui', 'Big Island', 'Kauai', 'Molokai', 'Lanai']
+
+const rangeOption = ['0', '1', '2', '3', '4', '5+']
+
+const airlines = ['Hawaiian', 'United', 'Delta', 'Virgin America', 'Alaska', 'jetBlue', 'Korean Air', 'Japan Airlines', 'ANA']
+
 class FormPage extends Component {
 
   render() {
-    const { fields: { firstName, lastName, email, address, city }, handleSubmit } = this.props;
+    const { fields: { firstName, lastName, address, city, state, zip, hotel, island, phone, email, party, airline, number }, handleSubmit } = this.props;
 
     return (
       <div>
@@ -22,16 +28,67 @@ class FormPage extends Component {
             </div>
           </div>
           <div>
-            <label>Street Address</label>
-            <input type="text" placeholder="Address" {...address}></input>
-          </div>
-          <div>
+            <label>Home Address</label>
+            <div>
+              <input type="text" placeholder="Address" {...address}></input>
+            </div>
             <label>City</label>
-            <input type="text" placeholder="City" {...city}></input>
+            <div>
+              <input type="text" placeholder="City" {...city}></input>
+            </div>
+            <label>State</label>
+            <div>
+              <input type="text" placeholder="State" {...state}></input>
+            </div>
+            <label>Zip</label>
+            <div>
+              <input type="text" placeholder="Zip" {...zip}></input>
+            </div>
           </div>
           <div>
+            <label>Name of Hotel/Lodging</label>
+            <div>
+              <input type="text" placeholder="Hotel" {...hotel}></input>
+            </div>
+            <label>Island</label>
+            <div>
+              <select {...island}>
+                <option value="">Select an Island</option>
+                {islands.map(islandOption => <option value={islandOption} key={islandOption}>{islandOption}</option>)}
+              </select>
+            </div>
+          </div>
+          <div>
+            <label>Phone Number</label>
+            <div>
+              <input type="int" placeholder="(555) 555-5555" {...phone}></input>
+            </div>
             <label>Email</label>
-            <input type="email" placeholder="Email" {...email}></input>
+            <div>
+              <input type="email" placeholder="Email" {...email}></input>
+            </div>
+          </div>
+          <div>
+            <label>Size of Party</label>
+            <div>
+              <select {...party}>
+                <option value="">0</option>
+                {rangeOption.map(partyRange => <option value={partyRange} key={partyRange}>{partyRange}</option>)}
+              </select>
+            </div>
+          </div>
+          <div>
+            <label>Airline</label>
+            <div>
+              <select {...airline}>
+                <option value="">Select Airline</option>
+                {airlines.map(airlineOption => <option value={airlineOption} key={airlineOption}>{airlineOption}</option>)}
+              </select>
+            </div>
+            <lable>Flight Number</lable>
+            <div>
+              <input type="int" placeholder="Flight Number" {...number}></input>
+            </div>
           </div>
           <button type="submit">Submit</button>
         </form>
@@ -42,5 +99,5 @@ class FormPage extends Component {
 
 export default reduxForm({
   form: 'departmentOfAg',
-  fields: ['firstName', 'lastName', 'email', 'address', 'city']
+  fields: ['firstName', 'lastName', 'address', 'city', 'state', 'zip', 'hotel', 'island', 'phone', 'email', 'party', 'airline', 'number']
 })(FormPage);
